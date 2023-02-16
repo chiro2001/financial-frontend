8710b3216de76a9509949fe4902f2a067a1e188f
Chiro Liang <Chiro2001@163.com>
2023-02-16 15:45:50 +0800

    replace fint

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use egui::{FontData, FontDefinitions, FontFamily};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
@@ -23,8 +25,19 @@ impl Default for FinancialAnalysis {
 impl FinancialAnalysis {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        // load chinese font
+        let mut fonts = FontDefinitions::default();
+        let font_name = "MiLanTing";
+        fonts.font_data.insert(
+            font_name.to_owned(),
+            FontData::from_static(include_bytes!("../assets/MiLanTing.ttf")),
+        );
+        fonts
+            .families
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, font_name.to_owned());
+        cc.egui_ctx.set_fonts(fonts);
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
@@ -37,11 +50,6 @@ impl FinancialAnalysis {
 }
 
 impl eframe::App for FinancialAnalysis {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
@@ -112,5 +120,10 @@ impl eframe::App for FinancialAnalysis {
                 ui.label("You would normally choose either panels OR windows.");
             });
         }
+    }
+
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
     }
 }
