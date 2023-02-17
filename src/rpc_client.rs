325fb386b983743eeaa6b95365d8bb0eb0544437
Chiro Liang <Chiro2001@163.com>
2023-02-17 10:59:38 +0800

    client test

--- src/rpc_client.rs
+++ src/rpc_client.rs
@@ -24,8 +24,25 @@ pub async fn get_client() -> Result<ApiClient> {
     use tokio_serde::formats::Json;
     use tarpc::client;
     use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-    let transport = tarpc::serde_transport::tcp::connect(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), API_PORT), Json::default);
+    let transport = tarpc::serde_transport::tcp::connect(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), rpc::API_PORT), Json::default);
     let client = ApiClient::new(client::Config::default(), transport.await?).spawn();
     info!("connected");
     Ok(client)
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+    use crate::rpc_client::get_client;
+
+    #[test]
+    fn client_test() -> Result<()> {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_io().build()?.block_on(async {
+            let client = get_client().await.unwrap();
+            let a = client.ping(tarpc::context::current()).await.unwrap().unwrap();
+            println!("result: {:?}", a);
+        });
+        Ok(())
+    }
 }
