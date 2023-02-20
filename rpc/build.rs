eee29e39a46ee3b131af1c82f102a2f0ed51cea5
Chiro Liang <Chiro2001@163.com>
2023-02-20 10:35:36 +0800

    remove grpc repo

--- rpc/build.rs
+++ rpc/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("financial-analysis-grpc/api.proto")?;
+    tonic_build::compile_protos("proto/api.proto")?;
     Ok(())
 }
